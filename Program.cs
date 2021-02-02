bf2472ef9c6f6f7f83a036f0080a76f562663505
Sergio Guillen <sagssky@gmail.com>
2021-02-02 17:19:35 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace Exercise1_CSharpAutomation
+{
+    class Program
+    {
+        IWebDriver driver;
+        public IWebDriver SetUpDriver() {
+            driver = new ChromeDriver(@"C:\Webdrivers\");
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(15);
+            return driver;
+        }
+
+        public void Click(IWebElement element) {
+            element.Click();
+        }
+
+        public void SendText(IWebElement element, string value) {
+            element.SendKeys(value);
+        }
+        static void Main(string[] args) {
+            IWebDriver Browser;
+            IWebElement element;
+            Program program = new Program();
+            Browser = program.SetUpDriver();
+            Browser.Url = "https://www.google.com";
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
